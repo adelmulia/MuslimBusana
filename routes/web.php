66aaa37f601cee35930c305501bf2ec72834afe7
sahrulguns <sahrulgunawan2519@gmail.com>
2023-02-28 11:38:46 +0700

    next authentication

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,4 @@ Route::prefix('admin')
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
